89cf850748d9899fe56db02d6cb57f471b29fc9f
DangerDay <rixochet@gmail.com>
2014-12-09 17:19:30 -0600

    ngrok, webstorm files, screenKeyboard
    - ngrok is a tunnel service for us to use in place of lt not working
    - commiting webstorm files so we get some level of intellisense
    - started on screen keyboard for player name input, this is going to
    have to be replaced with html and css since as soon as the player hits
    the delete key the browser goes back to the previous page

---
+++ artillery-game/client/screenKeyboard.js
@@ -0,0 +1,15 @@
+
+screenKeyboard = function (game)
+{
+    //game = game;
+    //displayText = game.add.text(game.width / 2, 0, this.text, {fontSize: '32px', fill: '#000'});
+    //
+    ////here you define what happens if any key is pressed, default is null
+    //game.input.keyboard.onDownCallback = function(e) {
+    //    //for demonstration, next line prints the keyCode to console
+    //    console.log(e.keyCode.toString());
+    //    displayText.text += String.fromCharCode(e.keyCode);
+    //}
+
+    window.prompt("Enter your name", "Steve");
+}
